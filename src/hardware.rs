7c13813c68e5bfe5b42f60fcb04c0191bb54ccc9
Graham Christensen <graham@grahamc.com>
2023-02-24 08:55:56 -0500

    hardware: respect category minimums

--- src/hardware.rs
+++ src/hardware.rs
@@ -85,7 +85,7 @@ pub async fn get_desired_hardware(
     for (system, sizes) in buckets.iter() {
         for (size, runnable) in sizes.iter() {
             if let Some(category) = categories.get(&system).and_then(|e| e.get(&size)) {
-                let wanted = max(1, runnable / category.divisor);
+                let wanted = max(category.minimum, runnable / category.divisor);
                 if category.plans.is_empty() {
                     println!(
                         "WARNING: {:?}/{:?}'s hardwarecategory has no plans",
